6a5296bbf03ed8ba42f635e14e6ae6cdfec48a85
ahj-ai <ajay_work@icloud.com>
2025-05-23 22:32:17 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,39 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  env: {
+    // Expose these environment variables to the client-side
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+  // Required for webpack to include environment variables in the build
+  webpack: (config) => {
+    // Exclude node_modules from being processed by babel
+    config.module.rules.push({
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    });
+    return config;
+  },
+  // Enable server-side environment variables
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET,
+  },
+  // Environment variables that will be available on both server and client
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 }
 
 module.exports = nextConfig
